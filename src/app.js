0896a33d8e72dc60e3dbff8d5b0c925e5cae9e67
Naqueeb <2021ugcs037@nitjsr.ac.in>
2024-08-01 17:34:37 +0530

    "Added playlist router and controller with CRUD operations and additional functionality"

--- src/app.js
+++ src/app.js
@@ -16,6 +16,7 @@ import cookieParser from 'cookie-parser'
  import videoRouter from "./routes/video.routes.js"
  import likeRouter from "./routes/like.routes.js"
  import commentRouter from "./routes/comment.routes.js"
+ import playlistRouter from "./routes/playlist.routes.js"
 
  //routes declaration
  app.use("/api/v1/users",userRouter) // here middleware .use is used wrna app.get likhte agr same file me likhte to
@@ -23,6 +24,7 @@ import cookieParser from 'cookie-parser'
  app.use("/api/v1/videos",videoRouter) 
  app.use("/api/v1/likes",likeRouter)
  app.use("/api/v1/comments",commentRouter)
+ app.use("/api/v1/playlists",playlistRouter)
  export {app}
 
 
